075ca0edbe1e5b6e05a93e2de4e9eb39bc6d8f22
Gael Cadoret <gael.cadoret@ingin.fr>
2016-01-17 23:58:31 +0100

    2nd commit

---
+++ main.js
@@ -0,0 +1,18 @@
+/**
+ * Created by Gael on 17/01/2016.
+ */
+
+function MyClass() {};
+
+MyClass.prototype = {
+
+    init: function() {
+        console.log("[MyClass] init()");
+    }
+
+};
+
+document.onload = function() {
+    var instanceOfMyClass = new MyClass();
+    instanceOfMyClass.init();
+};
